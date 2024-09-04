0f7d8bf4eb43b46f1b9ce37b0dc9fc5bbdf5a137
Miguel Angel Perez Souza <mpsouza@gradiant.org>
2024-09-04 10:28:08 +0200

    feat: CLAHE filter for images

--- mmcls/datasets/pipelines/transforms.py
+++ mmcls/datasets/pipelines/transforms.py
@@ -8,6 +8,7 @@ from typing import Sequence
 
 import mmcv
 import numpy as np
+import cv2
 
 from ..builder import PIPELINES
 from .compose import Compose
@@ -16,6 +17,34 @@ try:
     import albumentations
 except ImportError:
     albumentations = None
+
+
+@PIPELINES.register_module()
+class CLAHE(object):
+    def __init__(self, clipLimit: float = 3.0, gridSize: tuple = (8, 8)) -> None:
+        if isinstance(gridSize, tuple) and len(gridSize) == 2:
+            self.gridSize = gridSize
+        elif isinstance(gridSize, int):
+            self.gridSize = (gridSize, gridSize)
+        else:
+            assert isinstance(gridSize, tuple), "gridsize should be a tuple of length 2 or an int"
+        assert isinstance(clipLimit, float)
+        self.clipLimit = clipLimit
+        self.clahe = cv2.createCLAHE(clipLimit=self.clipLimit,
+                                     tileGridSize=self.gridSize)
+
+    def __call__(self, results):
+        for key in results.get('img_fields', ['img']):
+            img = results[key]
+            # print(type(img), img.dtype)
+            # img = np.rint(img)
+            # img = np.uint16(img)
+            img[..., 0] = self.clahe.apply(img[..., 0])
+            img[..., 1] = self.clahe.apply(img[..., 1])
+            img[..., 2] = self.clahe.apply(img[..., 2])
+            img_out = np.float32(img)
+            results[key] = img_out
+            return results
 
 
 @PIPELINES.register_module()
