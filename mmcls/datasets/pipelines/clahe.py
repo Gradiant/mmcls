ddb4ab1d9c948f91fe513e84d1f84d0516c3f767
Miguel Angel Perez Souza <mpsouza@gradiant.org>
2024-10-24 09:51:45 +0200

    fix: clipLimit assert not giving information

--- mmcls/datasets/pipelines/clahe.py
+++ mmcls/datasets/pipelines/clahe.py
@@ -1,18 +1,18 @@
-from mmcls.datasets import PIPELINES
+from ..builder import PIPELINES
 import cv2
 import numpy as np
 
 
 @PIPELINES.register_module()
 class CLAHE(object):
-    def __init__(self, clipLimit: float = 3.0, gridSize: tuple = (8, 8)) -> None:
+    def __init__(self, clipLimit: float = 3.0, gridSize: tuple = (10, 10)) -> None:
         if isinstance(gridSize, tuple) and len(gridSize) == 2:
             self.gridSize = gridSize
         elif isinstance(gridSize, int):
             self.gridSize = (gridSize, gridSize)
         else:
-            assert isinstance(gridSize, tuple), "gridsize should be a tuple of length 2 or an int"
-        assert isinstance(clipLimit, float)
+            assert isinstance(gridSize, tuple), "gridSize should be a tuple of length 2 or an int"
+        assert isinstance(clipLimit, float), "clipLimit should be of type float"
         self.clipLimit = clipLimit
         self.clahe = cv2.createCLAHE(clipLimit=self.clipLimit,
                                      tileGridSize=self.gridSize)
